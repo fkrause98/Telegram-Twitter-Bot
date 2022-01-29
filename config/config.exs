c5d95899c391578d0841b4819d95b8be62e16e2e
Francisco Krause Arnim <fkrausear@gmail.com>
2022-01-29 12:04:57 -0300

    Back to original config.exs

--- config/config.exs
+++ config/config.exs
@@ -1,5 +1,4 @@
 import Config
 
-config :ex_gram, token: System.fetch_env!("BOT_TOKEN"),
-  server: true,
-  http: [:inet6, port: System.get_env("PORT")]
+config :ex_gram, token: System.fetch_env!("BOT_TOKEN")
+
