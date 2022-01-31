c186a678c98aedd0b5aac609654d127aec5609f2
Francisco Krause Arnim <fkrausear@gmail.com>
2022-01-31 13:52:46 -0300

    Use URIs instead of Strings

--- lib/bot/modules/nitter.ex
+++ lib/bot/modules/nitter.ex
@@ -3,7 +3,7 @@ defmodule Nitter do
   def from_twitter(text) do
     cond do
       contains_twitter_url?(text) ->
-        text |> scan_and_get_urls |> filter_non_twitter |> urls_to_nitter |> maybe_remove_query
+        text |> scan_and_get_urls |> filter_non_twitter |> urls_to_nitter |> uris_to_text
 
       true ->
         {:error, :twitter_url_not_found}
@@ -13,9 +13,7 @@ defmodule Nitter do
   defp contains_twitter_url?(nil), do: false
   defp contains_twitter_url?(url), do: String.contains?(url, "twitter.com")
 
-  defp to_twitter(url) do
-    String.replace(url, "twitter.com", "nitter.net")
-  end
+  defp to_twitter(%URI{} = url), do: %{url | host: "nitter.net"}
 
   defp scan_and_get_urls(text) do
     Regex.scan(@url_regex, text)
@@ -30,15 +28,20 @@ defmodule Nitter do
 
   defp urls_to_nitter(urls) when is_list(urls) do
     urls
-    |> Enum.map(&URI.to_string/1)
     |> Enum.map(&to_twitter/1)
+    |> Enum.map(&maybe_remove_query/1)
+  end
+
+  defp uris_to_text(urls) when is_list(urls) do
+    urls
+    |> Enum.map(&URI.to_string/1)
     |> Enum.join("\n")
   end
 
-  defp maybe_remove_query(url) do
-    case String.split(url, "?") do
-      [link, _q_params] -> link
-      [link] -> link
+  defp maybe_remove_query(%URI{query: query} = url) do
+    case query do
+      nil -> url
+      _non_nil_query -> %{url | query: nil}
     end
   end
 end
